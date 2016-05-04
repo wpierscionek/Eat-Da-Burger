c2a4ba7b39fd20d9212dcd5012b02738ae5b74a7
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-05-03 22:48:45 -0400

    5/3/2016

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
+
+var app = express(); //creating express server
+var PORT = process.env.PORT || 80; //setting up port which we use for app
+
+//below code comes from body-parser package that makes it easy for our server to interpret data sent to it
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+
+// the below points our server to a series of "route" files
+require('./app/routing/api-routes.js')(app); // path to the file
+require('./app/routing/html-routes.js')(app); // path to the file
+
+// the below code starts the server
+
+app.listen(PORT, function() {
+    console.log('App listening on PORT: ' + PORT);
+})
