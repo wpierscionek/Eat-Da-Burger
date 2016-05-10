15ce8e000796aa149b537e222f572d4d12536782
Wojciech Pierscionek <PierscionekW@gmail.com>
2016-05-10 10:37:19 -0400

    5/10/2016

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    // port: 3306, //standart port for mysql
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
+    port: 3306,
+    host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'behxlozpv741aybs',
+    password: 'rd8a9ifnbbrl7jlp',
     database: 'burgers_db'
 });
 // console.log("mysql connected");
